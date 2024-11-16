15fab20e73919690a09e2e7a62353b429db4feed
Luca Tassinari <luca.tassinari.2000@gmail.com>
2024-11-16 14:17:22 +0100

    feat(domain): add UserToken interface

---
+++ domain/src/main/kotlin/io/github/positionpal/notification/domain/UserToken.kt
@@ -0,0 +1,26 @@
+package io.github.positionpal.notification.domain
+
+import io.github.positionpal.entities.UserId
+
+/** A type alias for a token. */
+typealias Token = String
+
+/** A value object representing a token associated to a [UserId] used to send push notifications. */
+interface UserToken {
+
+    /** The [UserId] associated to this token. */
+    val userId: UserId
+
+    /** The token used to send push notifications. */
+    val token: Token
+
+    /** Factory companion object to create new instances of [UserToken]. */
+    companion object {
+        /** Creates a new [UserToken] for the given [userId] associated to [token]. */
+        operator fun invoke(userId: UserId, token: String): UserToken = UserTokenImpl(userId, token)
+    }
+}
+
+private data class UserTokenImpl(override val userId: UserId, override val token: Token) : UserToken {
+    override fun toString(): String = "UserToken(userId=${userId.username()}, token=$token)"
+}
