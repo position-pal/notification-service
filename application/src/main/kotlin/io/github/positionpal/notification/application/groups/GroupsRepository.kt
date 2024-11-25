edf03625aa20f718b0ce436fbaee35315be320a3
Luca Tassinari <luca.tassinari.2000@gmail.com>
2024-11-25 16:07:00 +0100

    docs(application): improve api documentation

--- application/src/main/kotlin/io/github/positionpal/notification/application/groups/GroupsRepository.kt
+++ application/src/main/kotlin/io/github/positionpal/notification/application/groups/GroupsRepository.kt
@@ -3,18 +3,30 @@ package io.github.positionpal.notification.application.groups
 import io.github.positionpal.entities.GroupId
 import io.github.positionpal.entities.UserId
 
-/** A repository to manage groups and their members. */
+/**
+ * A repository managing groups and their members CRUD operations.
+ */
 interface GroupsRepository {
 
-    /** Adds the given [userId] to the group with the specified [groupId]. */
+    /**
+     * Adds the given [userId] to the group with the specified [groupId].
+     * @return a [Result] indicating the success or failure of the operation.
+     */
     suspend fun addMember(groupId: GroupId, userId: UserId): Result<Unit>
 
-    /** Removes the given [userId] from the group with the specified [groupId]. */
+    /**
+     * Removes the given [userId] from the group with the specified [groupId].
+     * @return a [Result] indicating the success or failure of the operation.
+     */
     suspend fun removeMember(groupId: GroupId, userId: UserId): Result<Unit>
 
-    /** @return the members of the group with the specified [groupId]. */
+    /**
+     * @return a successful [Result] with all the members of the given [groupId] or a failed one in case of error.
+     */
     suspend fun getMembersOf(groupId: GroupId): Result<Set<UserId>>
 
-    /** @return the groups of the user with the specified [userId]. */
+    /**
+     * @return a successful [Result] with the groups of the given [userId] or a failed one in case of error.
+     */
     suspend fun getGroupsOf(userId: UserId): Result<Set<GroupId>>
 }
--- application/src/main/kotlin/io/github/positionpal/notification/application/tokens/UsersTokensService.kt
+++ application/src/main/kotlin/io/github/positionpal/notification/application/tokens/UsersTokensService.kt
@@ -4,12 +4,20 @@ import io.github.positionpal.entities.UserId
 import io.github.positionpal.notification.domain.Token
 import io.github.positionpal.notification.domain.UserToken
 
-/** A service to manage [UserToken]s. */
+/**
+ * A service to manage [UserToken]s.
+ */
 interface UsersTokensService {
 
-    /** Registers a new [userId] - [token] association. */
+    /**
+     * Registers a new [userId] - [token] association.
+     * @return a successful [Result] with the registered [UserToken] or a failed one in case of error.
+     */
     suspend fun register(userId: UserId, token: Token): Result<UserToken>
 
-    /** Invalidates the given [token] for the given [userId]. */
+    /**
+     * Invalidates the given [token] for the given [userId], i.e., removes the [userId] - [token] association.
+     * @return a [Result] indicating the success or failure of the operation.
+     */
     suspend fun invalidate(userId: UserId, token: Token): Result<Unit>
 }
